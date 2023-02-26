1b37e0a0b9d2be92dd56e9d3f2a5705f51a4a849
TmkDigitalLDN <88273097+TmkDigitalLDN@users.noreply.github.com>
2023-02-26 19:31:08 +0000

    latest

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 
 
   return (
-      <BrowserRouter basename="/home">
+      <BrowserRouter basename="/">
         <CustomScrollbar/>
         <Markup />
       </BrowserRouter>
