c84999dc8535c932f21a0a3c86c77665b2dc6a7a
TmkDigitalLDN <88273097+TmkDigitalLDN@users.noreply.github.com>
2023-02-26 19:27:45 +0000

    latest

--- src/markup/pages/Index.js
+++ src/markup/pages/Index.js
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 
 
 // react router dom
- /*import { Link } from 'react-router-dom';*/
+ import { Link } from 'react-router-dom';
 
 // sliders
 import TestimonialSlider from '../components/TestimonialSlider';
